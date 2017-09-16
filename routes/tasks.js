9cfec188396a3240706388bfa86dc5ab586f6054
Evan Kearney <evanjkearney@gmail.com>
2017-09-16 17:35:59 -0500

    Attach query to /tasks GET route

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,7 +3,22 @@ var pool = require('../modules/pool.js');
 
 router.get('/', function(req, res){
     console.log('querying database');
-    res.sendStatus(201);
+    pool.connect(function(error, client, done){
+        if (error){
+            console.log(error);
+            res.sendStatus(500);
+        }else{
+            client.query('SELECT * FROM tasks_todo', function(queryError, resultObj){
+                done();
+                if (queryError){
+                    console.log(queryError);
+                    res.sendStatus(500);
+                }else{
+                    res.send(resultObj.rows);
+                }
+            })
+        }
+    })
 })
 
 module.exports = router;
